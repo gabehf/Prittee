1a43cb6c4df5b78760888692f36355cb24d9e155
Gabe Farrell <gabe@mnrva.dev>
2024-04-02 08:46:42 +0000

    FIX: Update bug request form

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <Box width={600} className='flex flex-row justify-around text-sm text-neutral-500 border-t-2 border-neutral-600 p-4 mt-4'>
             <a className="hover:text-neutral-300" href="https://github.com/gabehf/Prittee" target="_blank">View the source on GitHub</a>
-            <a className="hover:text-neutral-300" href="https://forms.gle/a8BkH6yUDnzLJD938" target="_blank">Report a bug</a>
+            <a className="hover:text-neutral-300" href="https://forms.gle/YTBkxUk7mKsmMsvC6" target="_blank">Report a bug</a>
         </Box>
     )
 }
